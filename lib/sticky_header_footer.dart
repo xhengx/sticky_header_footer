71f7b0e68b53830838ebaff22ded95866487450c
xheng <liuxh006@qianhai.com.cn>
2019-11-27 17:05:22 +0800

    添加export

---
+++ lib/sticky_header_footer.dart
@@ -0,0 +1,7 @@
+// Copyright 2019 Xiaoheng Liu. All rights reserved.
+// Use of this source code is governed by a the MIT license that can be
+// found in the LICENSE file.
+library sticky_header_footer;
+
+export 'sticky_header_footer/widget.dart';
+export 'sticky_header_footer/render.dart';
